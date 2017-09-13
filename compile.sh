6db35eb8cc69e256c930476f574e406b6eb72433
dlofaro <dlofaro@gmu.edu>
2017-09-12 20:07:48 -0400

    Initial ShareLaTeX Import

---
+++ compile.sh
@@ -0,0 +1,25 @@
+rm *.bbl
+rm *.aux
+rm *.log
+rm *.blg
+rm *.out
+
+thefile=dlofaro-humanoids-2017-mds-ach
+pdflatex $thefile.tex
+bibtex $thefile.aux
+pdflatex $thefile.tex
+pdflatex $thefile.tex
+rm *.bbl
+rm *.aux
+rm *.log
+rm *.blg
+rm *.out
+
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/default     -dNOPAUSE -dQUIET -dBATCH -dDetectDuplicateImages     -dCompressFonts=true -r150 -sOutputFile=$thefile.small.pdf $thefile.pdf
+    evince $thefile.small.pdf
+    # Do something under GNU/Linux platform
+else 
+    open $thefile.pdf
+fi
+
